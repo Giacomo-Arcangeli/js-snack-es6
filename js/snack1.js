2a32f385488f5e6930a648d22436e8beae8ea5c6
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-14 09:43:31 +0200

    fix snack 1

--- js/snack1.js
+++ js/snack1.js
@@ -1,13 +1,10 @@
-const guestList = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+const names = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-placeCard = [];
-for ( let i=0; i < guestList.length; i++){
-    placeCard = [
-        {
-            table: 'Tavolo Vip',
-            name: guestList[i],
-            place: guestList.length
-        }
-    ]
-} 
-console.log(placeCard);
+const guests = names.map((name , i) => {
+    const guest = {
+        table: 'Tavolo Vip',
+        name: name,
+        place: i + 1
+    } 
+    console.log(guest);
+});
